ca196535330604e1ff210033e51c6c42330ea03c
Vitor Daniel <vitor036daniel@gmail.com>
2025-01-06 16:07:30 -0300

    add ssl

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
 import preact from "@preact/preset-vite"
 import path from "node:path"
+import basicSsl from "@vitejs/plugin-basic-ssl"
 
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [basicSsl(), preact()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
